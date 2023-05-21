78141973793c86a7ba15899454c09e79b4706b5d
Ahmad Fauzan <akisearu17@192.168.1.8>
2023-05-21 11:04:17 +0800

    handle root and 404 route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  root to: redirect('/witch-calculator')
+
   get '/witch-calculator', to: 'witch_calculator#index'
   post '/calculate-average-kills', to: 'witch_calculator#calculate_average_kills'
   get '/witch-calculator/result', to: 'witch_calculator#result', as: 'witch_calculator_result'
+
+  get '*path' => redirect('/witch-calculator')
 end
